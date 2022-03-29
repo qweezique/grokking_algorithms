cc7ccb2031c0c97047f2e165a8b0844e63186df2
qweezique <qweezique@gmail.com>
2022-03-30 00:00:36 +0500

    Initial commit

---
+++ src/main/java/binarySearch/BinarySearch.java
@@ -0,0 +1,40 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        {
+
+            String[] array = new String[]{"arr", "bff", "ccc"};
+            Integer searchPos = BinarySearch.search(array, "ccc");
+            System.out.println("SEARCH_POS " + searchPos);
+
+
+            //Binary Search as static method in Arrays
+            int index = Arrays.binarySearch(array, "ccc");
+            System.out.println("INDEX = " + index);
+        }
+    }
+
+    public static Integer search(Comparable[] array, Comparable itemSearch) {
+        int indexMin = 0;
+        int indexMax = array.length - 1;
+
+        while (indexMin <= indexMax) {
+            int indexMid = (indexMin + indexMax) / 2;
+
+            Comparable itemMid = array[indexMid];
+            Integer resultCompare = itemMid.compareTo(itemSearch);
+
+            if (resultCompare == 0) {
+                return indexMid;
+            } else if (resultCompare > 0) {
+                indexMax = indexMid - 1;
+            } else {
+                indexMin = indexMid + 1;
+            }
+        }
+        return null;
+    }
+}
